c15bb9ba622eaadf912f59fe2dbed0e6f14ec840
Rui Pedro Moreira <rpdorm@gmail.com>
2021-08-25 01:45:03 +0100

    new iteration

---
+++ assets/js/data.js
@@ -0,0 +1,315 @@
+data = {
+	"year": 2021,
+	"series": {
+		"0": {
+			"name": "Formula 1",
+			"symbol": "F1",
+			"events": {
+				"0": {
+					"round": 12,
+					"name": "Belgian Grand Prix",
+					"circuit": 0,
+					"sessions":{
+						"0": {
+							"name": "Free Practice 1",
+							"symbol": "FP1",
+							"timestamp": 1630056600,
+							"length": 3600,
+							"laps": false
+						},
+						"1": {
+							"name": "Free Practice 2",
+							"symbol": "FP2",
+							"timestamp": 1630069200,
+							"length": 3600,
+							"laps": false
+						},
+						"2": {
+							"name": "Free Practice 3",
+							"symbol": "FP3",
+							"timestamp": 1630144800,
+							"length": 3600,
+							"laps": false
+						},
+						"3": {
+							"name": "Qualifying",
+							"symbol": "Q",
+							"timestamp": 1630155600,
+							"length": 3600,
+							"laps": false
+						},
+						"4": {
+							"name": "Race",
+							"symbol": "R",
+							"timestamp": 1630242000,
+							"length": 7200,
+							"laps": 44
+						}
+					}
+				},
+				"1": {
+					"round": 13,
+					"name": "Dutch Grand Prix",
+					"circuit": 1,
+					"sessions":{
+						"0": {
+							"name": "Free Practice 1",
+							"symbol": "FP1",
+							"timestamp": 1630665000,
+							"length": 3600,
+							"laps": false
+						},
+						"1": {
+							"name": "Free Practice 2",
+							"symbol": "FP2",
+							"timestamp": 1630674000,
+							"length": 3600,
+							"laps": false
+						},
+						"2": {
+							"name": "Free Practice 3",
+							"symbol": "FP3",
+							"timestamp": 1630749600,
+							"length": 3600,
+							"laps": false
+						},
+						"3": {
+							"name": "Qualifying",
+							"symbol": "Q",
+							"timestamp": 1630760400,
+							"length": 3600,
+							"laps": false
+						},
+						"4": {
+							"name": "Race",
+							"symbol": "R",
+							"timestamp": 1630846800,
+							"length": 7200,
+							"laps": 72
+						}
+					}
+				},
+				"2": {
+					"round": 14,
+					"name": "Italian Grand Prix",
+					"circuit": 2,
+					"sessions":{
+						"0": {
+							"name": "Free Practice 1",
+							"symbol": "FP1",
+							"timestamp": 1631277000,
+							"length": 3600,
+							"laps": false
+						},
+						"1": {
+							"name": "Qualifying",
+							"symbol": "Q",
+							"timestamp": 1631289600,
+							"length": 3600,
+							"laps": false
+						},
+						"2": {
+							"name": "Free Practice 2",
+							"symbol": "FP2",
+							"timestamp": 1631354400,
+							"length": 3600,
+							"laps": false
+						},
+						"3": {
+							"name": "Sprint",
+							"symbol": "S",
+							"timestamp": 1631370600,
+							"length": 3600,
+							"laps": false
+						},
+						"4": {
+							"name": "Race",
+							"symbol": "R",
+							"timestamp": 1631451600,
+							"length": 7200,
+							"laps": 53
+						}
+					}
+				},
+				"2": {
+					"round": 15,
+					"name": "Russian Grand Prix",
+					"circuit": 5,
+					"sessions":{
+						"0": {
+							"name": "Free Practice 1",
+							"symbol": "FP1",
+							"timestamp": 1632472200,
+							"length": 3600,
+							"laps": false
+						},
+						"1": {
+							"name": "Free Practice 2",
+							"symbol": "FP2",
+							"timestamp": 1632484800,
+							"length": 3600,
+							"laps": false
+						},
+						"2": {
+							"name": "Free Practice 3",
+							"symbol": "FP3",
+							"timestamp": 1632560400,
+							"length": 3600,
+							"laps": false
+						},
+						"3": {
+							"name": "Qualifying",
+							"symbol": "Q",
+							"timestamp": 1632571200,
+							"length": 3600,
+							"laps": false
+						},
+						"4": {
+							"name": "Race",
+							"symbol": "R",
+							"timestamp": 1632657600,
+							"length": 7200,
+							"laps": 53
+						}
+					}
+				}
+			}
+		},
+		"1": {
+			"name": "IndyCar Series",
+			"symbol": "IndyCar",
+			"events": {
+				"0": {
+					"round": 14,
+					"name": "Grand Prix of Portland",
+					"circuit": 3,
+					"sessions":{
+						"0": {
+							"name": "Practice 1",
+							"symbol": "P",
+							"timestamp": 1631376000,
+							"length": 4500,
+							"laps": false
+						},
+						"1": {
+							"name": "Qualifications",
+							"symbol": "Q",
+							"timestamp": 1631387700,
+							"length": 4500,
+							"laps": false
+						},
+						"2": {
+							"name": "Final Practice",
+							"symbol": "P",
+							"timestamp": 1631398500,
+							"length": 1800,
+							"laps": false
+						},
+						"3": {
+							"name": "Race",
+							"symbol": "R",
+							"timestamp": 1631473200,
+							"length": 10800,
+							"laps": 102
+						}
+					}
+				}
+			}
+		},
+		"2": {
+			"name": "MotoGP",
+			"symbol": "MotoGP",
+			"events": {
+				"0": {
+					"round": 12,
+					"name": "British Grand Prix",
+					"circuit": 4,
+					"sessions":{
+						"0": {
+							"name": "Free Practice 1",
+							"symbol": "FP1",
+							"timestamp": 1630054500,
+							"length": 2700,
+							"laps": false
+						},
+						"1": {
+							"name": "Free Practice 2",
+							"symbol": "FP2",
+							"timestamp": 1630069800,
+							"length": 2700,
+							"laps": false
+						},
+						"2": {
+							"name": "Free Practice 3",
+							"symbol": "FP3",
+							"timestamp": 1630140900,
+							"length": 2700,
+							"laps": false
+						},
+						"3": {
+							"name": "Free Practice 4",
+							"symbol": "FP4",
+							"timestamp": 1630153800,
+							"length": 2700,
+							"laps": false
+						},
+						"4": {
+							"name": "Qualifying 1",
+							"symbol": "Q1",
+							"timestamp": 1630156200,
+							"length": 900,
+							"laps": false
+						},
+						"5": {
+							"name": "Qualifying 2",
+							"symbol": "Q2",
+							"timestamp": 1630157700,
+							"length": 900,
+							"laps": false
+						},
+						"6": {
+							"name": "Warm Up",
+							"symbol": "WU",
+							"timestamp": 1630225800,
+							"length": 1200,
+							"laps": false
+						},
+						"7": {
+							"name": "Race",
+							"symbol": "R",
+							"timestamp": 1630238400,
+							"length": 7200,
+							"laps": false
+						}
+					}
+				}
+			}
+		}
+	},
+	"circuits": {
+		"0": {
+			"name": "Spa-Francorchamps",
+			"location": "Stavelot, Belgium"
+		},
+		"1": {
+			"name": "Zandvoort",
+			"location": "Zandvoort, Netherlands"
+		},
+		"2": {
+			"name": "Monza",
+			"location": "Monza, Italy"
+		},
+		"3": {
+			"name": "Portland International Raceway",
+			"location": "Portland, Oregon"
+		},
+		"4": {
+			"name": "Silverstone",
+			"location": "Silverstone, United Kingdom"
+		},
+		"5": {
+			"name": "Sochi",
+			"location": "Sochi, Russia"
+		}
+	}
+}
---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+now = Math.floor(Date.now()/1000);
+upcoming = now+2592000;
+today = moment().isoWeekday;
+series = data.series;
+circuits = data.circuits;
+maxHeight = 0;
+for (i=0; i<Object.keys(series).length; i++) {
+	events = series[i].events;
+	for (j=0; j<Object.keys(events).length; j++) {
+		t=Object.keys(series[i].events[j].sessions).length-1;
+		timestamp = series[i].events[j].sessions[t].timestamp+129600;
+		shatimestamp = sha256("sha" + series[i] + timestamp);
+		if (timestamp >= now && timestamp <= upcoming) {
+			name = series[i].events[j].name;
+			circuit = circuits[series[i].events[j].circuit];
+			$(".eventlist").append("<div  id='" + timestamp + "' class='card sortme' style='width: 18rem;'><div class=' card-header'><img class='card-img-top' src='assets/png/" + series[i].symbol + ".png'><h5 class='card-title text-center'>" + name + "</h5><h6 class='card-subtitle mb-2 text-muted text-center'>" + circuit.name + "</h6></div></div>");
+			sessions = series[i].events[j].sessions;
+			/*$("#" + timestamp).append("<p class='card-subtitle mb-2 text-muted'>Round " + series[i].events[j].round + "</p>");
+			$("#" + timestamp).append("<p class='card-subtitle mb-2 text-muted'>" + circuit.name + "</p>");*/
+			sha = sha256(series[i]+name+circuit);
+			$("#" + timestamp).append("<ul class='list-group list-group-flush' id='" + sha + "'>");
+			numberOfSessions=Object.keys(sessions).length;
+			if (maxHeight < numberOfSessions) { maxHeight = numberOfSessions; }
+			for (k=0; k<numberOfSessions; k++) {
+				session = series[i].events[j].sessions[k].name;
+				length = series[i].events[j].sessions[k].length;
+				sessionTimeUnix=Math.floor(series[i].events[j].sessions[k].timestamp*1000);
+				sessionTime = moment(sessionTimeUnix).format('ddd MMM Do YYYY HH:mm');
+				if (sessionTimeUnix+length > now) { del=""; }
+				else { del="<del>"; }
+				$("#" + sha).append("<li class='bg-light list-group-item'><p class='card-subtitle text-muted'><small>" + del + sessionTime + "</small></p><h6>" + del + session + "</h6></li>");
+				
+			}
+			$("#" + shatimestamp).append("</ul>");
+		}
+	}
+}
+
+var mylist = $('.eventlist');
+var listitems = mylist.children('div').get();
+listitems.sort(function(a, b) {
+    var compA = $(a).attr('id').toUpperCase();
+    var compB = $(b).attr('id').toUpperCase();
+    return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+})
+$.each(listitems, function(idx, itm) {
+    mylist.append(itm);
+});
+
+$(document).ready(function () {
+	maxHeight = maxHeight * 50;
+	cardHeight = maxHeight+128;
+	$("ul").height(maxHeight);
+	$(".card").height(cardHeight);
+});
