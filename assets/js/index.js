b54576f6d43709437cc0d44baac43ff3afc16e55
rpdorm <rpdorm@gmail.com>
2023-12-20 06:41:30 +0000

    added filter by series

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 $(".live").hide();
 now = Math.floor(Date.now()/1000);
 if ($(location).attr("hash") == "#show1month") { nWeeks = 4; $("#show1month").addClass("btn-light"); }
-else if ($(location).attr("hash") == "#show12month") { nWeeks = 52; $("#show12month").addClass("btn-light"); }
-else { nWeeks = 8; $("#show2month").addClass("btn-light"); } // show 8 weeks ahead by default
+else if ($(location).attr("hash") == "#show2month") { nWeeks = 8; $("#show12month").addClass("btn-light"); }
+else { nWeeks = 52; $("#show12month").addClass("btn-light"); } // show 52 weeks ahead by default
 upcoming = now+691200*nWeeks;
 today = moment().isoWeekday;
 series = data.series;
@@ -11,6 +11,7 @@ nSessions = 0;
 nEvents = 0;
 nLive = 0;
 for (i=0; i<Object.keys(series).length; i++) {
+	$(".filter").append(`<li class="nav-item"><a href="#" id="${series[i].symbol}" class="nav-link text-white sortli" aria-current="page">${series[i].name}</a></li>`);
 	events = series[i].events;
 	imgtitle = series[i].name;
 	for (j=0; j<Object.keys(events).length; j++) {
@@ -27,7 +28,7 @@ for (i=0; i<Object.keys(series).length; i++) {
 				url = "#";
 				target = "_self";
 			}
-			$(".event-list-grid").append(`<article id='${timestamp}' class='${shatimestamp} sortme'><div class='header' id='${shatimestamp}'><h5 class='title'><img class='img-top' title='${imgtitle}' src='assets/png/series/${series[i].symbol}.png'>${name}</h5><hr><a href='${url}' target='${target}'><h6 class='mb-2'><img class='img-flag' src='assets/png/flags/${flag}.png'>${venue.name}</h6></a></div></article>`);
+			$(".event-list-grid").append(`<article id='${timestamp}' class='${shatimestamp} ${series[i].symbol} sortme'><div class='header' id='${shatimestamp}'><h5 class='title'><img class='img-top' title='${imgtitle}' src='assets/png/series/${series[i].symbol}.png'>${name}</h5><hr><a href='${url}' target='${target}'><h6 class='mb-2'><img class='img-flag' src='assets/png/flags/${flag}.png'>${venue.name}</h6></a></div></article>`);
 			nEvents++;
 			sessions = series[i].events[j].sessions;
 			sha = sha256(`${series[i].name}-${i}-${name}-${venue.name}`);
@@ -42,6 +43,7 @@ for (i=0; i<Object.keys(series).length; i++) {
 				sessionTimeUnix=Math.floor(series[i].events[j].sessions[k].timestamp*1000);
 				sessionTime = moment(sessionTimeUnix).format('HH:mm');
 				sessionDate = moment(sessionTimeUnix).format('ddd, MMM Do');
+				year = moment(sessionTimeUnix).format('YYYY');
 				if (sessionTimeUnix/1000+length > now) {
 					del="";
 					if (sessionTimeUnix/1000 < now) {
@@ -66,15 +68,44 @@ for (i=0; i<Object.keys(series).length; i++) {
 }
 $(".nEvents").append(nEvents);
 $(".nSessions").append(nSessions);
-$(".nWeeks").append(nWeeks);
+$(".Year").append(year);
 
-var mylist = $('.event-list-grid');
-var listitems = mylist.children('article').get();
-listitems.sort(function(a, b) {
-    var compA = $(a).attr('id').toUpperCase();
-    var compB = $(b).attr('id').toUpperCase();
-    return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
-})
-$.each(listitems, function(idx, itm) {
-    mylist.append(itm);
+
+$(document).ready(function(){
+	// SORT EVENTS LIST
+	var mylist = $('.event-list-grid');
+	var listitems = mylist.children('article').get();
+	listitems.sort(function(a, b) {
+	    var compA = $(a).attr('id').toUpperCase();
+	    var compB = $(b).attr('id').toUpperCase();
+	    return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+	})
+	$.each(listitems, function(idx, itm) {
+	    mylist.append(itm);
+	});
+
+	// SORT LI
+	function sortUL(selector) {
+	    $(selector).children("li.sortli").sort(function(a, b) {
+	        var upA = $(a).text().toUpperCase();
+	        var upB = $(b).text().toUpperCase();
+	        return (upA < upB) ? -1 : (upA > upB) ? 1 : 0;
+	    }).appendTo(selector);
+	}
+	sortUL("ul.filter");
+
+	// FILTER CLICK
+	$(".nav-link").click(function() {
+		if ($(this).attr('id') == "show-all") {
+			$("article").show();
+			$("a.active").removeClass("active");
+			$(this).addClass("active");
+		}
+		else {
+			$("article").hide();
+			$("article." + $(this).attr('id')).show();
+			$("a.active").removeClass("active");
+			$(this).addClass("active");
+		}
+	});
 });
